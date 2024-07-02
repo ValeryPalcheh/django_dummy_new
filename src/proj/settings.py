1498a1e1a2de9b423fe4fb763fe7eb2597b4b8c4
V <pvv.1983.minsk@gmail.com>
2024-07-02 20:50:23 +0300

    создание local_settings

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j2aqw@+-mjjpzy)e2g9ur5xq@0op81-1^)deyef=@kbq!12p!t'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
